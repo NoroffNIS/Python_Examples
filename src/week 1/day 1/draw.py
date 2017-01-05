a5820a0314d727b17bd81c31501edb69a265584f
rbrage <brage89@gmail.com>
2017-01-05 15:05:45 +0100

    Add house to drawings

--- src/week 1/day 1/draw.py
+++ src/week 1/day 1/draw.py
@@ -1,6 +1,20 @@
 print('   /\ ')
 print('  /  \\')
 print(' /    \\')
-print('/\t\\')
+print('/______\\')
+print('|      |')
+print('|      |')
+print('|      |')
+print('|______|')
 
+house = {  'roof_1':'   /\ ',
+           'roof_2':'  /  \\',
+           'roof_3':' /    \\',
+           'roof_4':'/      \\',
+           'wall':'|      |',
+           'base_1':'|------|',
+           'base_2':'|______|'}
 
+print(house['roof_1'], house['roof_2'], house['roof_3'],house['roof_4'],
+      house['base_1'], house['wall'], house['wall'],house['wall'],
+      house['base_2'],sep='\n')
