a63791646106dbd7635520ca309a5919d66681a7
rbrage <brage89@gmail.com>
2017-01-11 12:39:03 +0100

    Add import example

---
+++ src/week 3/day 2/Pets_say.py
@@ -0,0 +1,12 @@
+
+def purr(pet='A Cat'):
+    print(pet, 'says MEOW!')
+
+
+def lick(pet='A Cat'):
+    print(pet, 'drinks milk!')
+
+
+def nap(pet='A Cat'):
+    print(pet, 'sleeps by the fire!')
+
---
+++ src/week 3/day 2/Storing Functions.py
@@ -0,0 +1,6 @@
+import Pets_say
+
+Pets_say.purr('Dog')
+Pets_say.lick('Dog')
+Pets_say.nap('Dog')
+
