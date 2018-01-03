ffab09002d3549e6f440a303fccc0fd61bb80472
Brage Roalkvam <brage89@gmail.com>
2018-01-03 14:26:19 +0100

    example from 3.2.2018

---
+++ src/week 1/day 1/hello_world_2018.py
@@ -0,0 +1,8 @@
+var = 2.0
+var1 = 2
+print('Hello world!', var,var1, sep='--', end='****')
+print('Hello world!'*10)
+input_user = input('Whats your name?')
+print(type(var))
+print(type(input_user))
+print(input_user)
